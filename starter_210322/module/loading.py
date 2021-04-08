b4fd8b31260f7afb78660b9bcb7f8f4f2ee5519a
ShinAnase <F8hs02a1kX@gmail.com>
2021-04-08 21:36:44 +0900

    base

--- starter_210322/module/loading.py
+++ starter_210322/module/loading.py
@@ -12,7 +12,18 @@ def get_train_file_path(image_id):
     )
 
 
+def get_test_file_path(image_id):
+    return setting.INPUT + "/test/{}/{}/{}/{}.png".format(
+        image_id[0], image_id[1], image_id[2], image_id
+    )
+
+
 train_ = pd.read_pickle(setting.OUTPUT + '/preprocessingTrain/train2_pycharm.pkl')
 train_['file_path'] = train_['image_id'].apply(get_train_file_path)
 train = train_.copy()
 del train_
+
+test_ = pd.read_csv(setting.INPUT + '/sample_submission.csv')
+test_['file_path'] = test_['image_id'].apply(get_test_file_path)
+test = test_.copy()
+del test_
